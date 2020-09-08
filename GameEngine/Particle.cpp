b3fe6de607b6fde2f1a620ad6d576a2c1b23e118
louisgirard <louis.girard17@hotmail.com>
2020-09-08 11:35:52 +0200

    add particle class

---
+++ GameEngine/Particle.cpp
@@ -0,0 +1,104 @@
+#include "Particle.h"
+
+Vector3 Particle::getPosition() const
+{
+	return position;
+}
+
+void Particle::setPosition(const Vector3& position)
+{
+	this->position = position;
+}
+
+void Particle::setPosition(const float x, const float y, const float z)
+{
+	position.x = x;
+	position.y = y;
+	position.z = z;
+}
+
+Vector3 Particle::getVelocity() const
+{
+	return velocity;
+}
+
+void Particle::setVelocity(const Vector3& position)
+{
+	this->velocity = velocity;
+}
+
+void Particle::setVelocity(const float x, const float y, const float z)
+{
+	velocity.x = x;
+	velocity.y = y;
+	velocity.z = z;
+}
+
+Vector3 Particle::getAcceleration() const
+{
+	return acceleration;
+}
+
+void Particle::setAcceleration(const Vector3& position)
+{
+	this->acceleration = acceleration;
+}
+
+void Particle::setAcceleration(const float x, const float y, const float z)
+{
+	acceleration.x = x;
+	acceleration.y = y;
+	acceleration.z = z;
+}
+
+float Particle::getDamping() const
+{
+	return damping;
+}
+
+void Particle::setDamping(const float damping)
+{
+	this->damping = damping;
+}
+
+float Particle::getInverseMass() const
+{
+	return inverseMass;
+}
+
+void Particle::setInverseMass(const float inverseMass)
+{
+	this->inverseMass = inverseMass;
+}
+
+float Particle::getMass() const
+{
+	if (inverseMass == 0) {
+		return std::numeric_limits<float>::max();
+	}
+	else {
+		return 1.0 / inverseMass;
+	}
+}
+
+void Particle::setMass(const float mass)
+{
+	if (mass == 0) {
+		return;
+	}
+	else {
+		inverseMass = 1.0 / mass;
+	}
+}
+
+void Particle::integrate(float time)
+{
+	// Do not integrate if infinite mass
+	if (inverseMass <= 0.0) { return; }
+
+	// Update the position
+	position += velocity * time;
+
+	// Update velocity
+	velocity = velocity * powf(damping, time) + acceleration * time;
+}
---
+++ GameEngine/Particle.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "Vector3.h"
+#include <limits>
+class Particle
+{
+private:
+	/* Holds the position of the particle */
+	Vector3 position;
+
+	/* Holds the velocity of the particle */
+	Vector3 velocity;
+
+	/* Holds the acceleration of the particle */
+	Vector3 acceleration;
+
+	/* Holds the damping applied to linear motion */
+	float damping;
+
+	/* Holds the inverse of the mass of the particle */
+	float inverseMass;
+
+public:
+	/* Returns the position of the particle */
+	Vector3 getPosition() const;
+
+	/* Sets the position of the particle */
+	void setPosition(const Vector3& position);
+	void setPosition(const float x, const float y, const float z);
+
+	/* Returns the velocity of the particle */
+	Vector3 getVelocity() const;
+
+	/* Sets the velocity of the particle */
+	void setVelocity(const Vector3& position);
+	void setVelocity(const float x, const float y, const float z);
+
+	/* Returns the acceleration of the particle */
+	Vector3 getAcceleration() const;
+
+	/* Sets the acceleration of the particle */
+	void setAcceleration(const Vector3& position);
+	void setAcceleration(const float x, const float y, const float z);
+
+	/* Returns the damping */
+	float getDamping() const;
+
+	/* Sets the damping */
+	void setDamping(const float damping);
+
+	/* Returns the inverse of the mass of the particle */
+	float getInverseMass() const;
+
+	/* Sets the inverse of the mass of the particle */
+	void setInverseMass(const float inverseMass);
+
+	/* Returns the mass of the particle */
+	float getMass() const;
+
+	/* Sets the mass of the particle */
+	void setMass(const float mass);
+
+	/* Integrate the particle */
+	void integrate(float time);
+};
+
