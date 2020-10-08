2bcb02895f616f726020e9ed9661cf885b4209ff
louisgirard <louis.girard17@hotmail.com>
2020-10-08 11:11:21 +0200

    Added drag force

---
+++ GameEngine/PhysicEngine/Src/ParticleDrag.cpp
@@ -0,0 +1,24 @@
+#include <PhysicEngine/Header/ParticleDrag.hpp>
+
+namespace PhysicEngine {
+
+	ParticleDrag::ParticleDrag(float p_k1, float p_k2)
+	{
+		_k1 = p_k1;
+		_k2 = p_k2;
+	}
+
+	void ParticleDrag::updateForce(CParticle* p_particle, double p_dt)
+	{
+		// Get normalized velocity of the particle
+		Vector3 normalizedVelocity = p_particle->getVelocity();
+		normalizedVelocity.normalize();
+
+		float magnitude = normalizedVelocity.magnitude();
+
+		// Calculate force
+		Vector3 force = normalizedVelocity * -1 * (_k1 * magnitude + _k2 * magnitude * magnitude);
+
+		p_particle->addForce(force);
+	}
+}
