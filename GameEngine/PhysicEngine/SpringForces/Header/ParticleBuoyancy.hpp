abacdae5643913a16c6021238349d33fab9716da
Anthony <anthony.david1512@yahoo.fr>
2020-10-27 20:06:55 +0100

    Corrected the way buoyency works

--- GameEngine/PhysicEngine/SpringForces/Header/ParticleBuoyancy.hpp
+++ GameEngine/PhysicEngine/SpringForces/Header/ParticleBuoyancy.hpp
@@ -13,11 +13,13 @@ namespace PhysicEngine {
 			float _volume;
 			/* The height of the water plane */
 			float _waterHeight;
+			/*Gravity acceleration*/
+			float _gravity;
 			/* Density of the liquid */
 			float _density;
 
 		public:
-			ParticleBuoyancy(float p_maxDepth, float p_volume, float p_waterHeight, float p_density = 1000.0f);
+			ParticleBuoyancy(float p_maxDepth, float p_volume, float p_waterHeight, float p_gravity, float p_density = 1000.0f);
 
 			/*Applies buoyancy force to a particle
 			* @param p_particle the particle to apply buoyancy on
